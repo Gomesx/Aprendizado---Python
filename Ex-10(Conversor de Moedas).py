ce11f495712392c787de3bd598e520c83754e92c
Gomesx <73998968+Gomesx@users.noreply.github.com>
2021-03-22 12:22:28 -0300

    Delete Ex-10(Conversor de Moedas).py

--- Ex-10(Conversor de Moedas).py
+++
@@ -1,6 +0,0 @@
-print('Seja bem-vindo ao convertor de moedas!')
-
-valore = float(input('Por favor, digite quantos reais você possui: '))
-valord = valore / 5.53 # Cotação do dolár 11/03/2021
-
-print(' O valor em doláres é de ${:.2f}'.format(valord))
