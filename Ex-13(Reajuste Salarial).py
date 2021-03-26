d5b2507b07984cb64fe5cc4dbbf33ef5e856922b
Gomesx <73998968+Gomesx@users.noreply.github.com>
2021-03-26 11:47:23 -0300

    Delete Ex-13(Reajuste Salarial).py

--- Ex-13(Reajuste Salarial).py
+++
@@ -1,8 +0,0 @@
-print('Olá, seja bem-vindo ao contador de salário.')
-
-sal = float(input('Digite o seu salário: R$'))
-
-r = sal * 0.15
-salt = sal + r
-
-print('Segundo os meus cálculos, o seu novo salário1 é de R$ {:.2f}'.format(salt))
